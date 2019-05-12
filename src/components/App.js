3afde6da2a8c85867ddf41c06a8743d836a0479b
Fred <sohnf90@yahoo.com>
2019-05-12 01:28:30 -0700

    Add pokemonListUrl and pokemonList to state and add click handler

--- src/components/App.js
+++ src/components/App.js
@@ -1,48 +1,49 @@
-import React, { Component } from 'react';
-import './styles/App.css';
-import axios from 'axios';
+import React, { Component } from "react";
+import "./styles/App.css";
+import axios from "axios";
 
-import PokeList from '../components/PokeList';
-import DetailView from '../components/DetailView';
-import Pokemon from '../Pokemon';
+import PokeList from "../components/PokeList";
+import DetailView from "../components/DetailView";
+import Pokemon from "../Pokemon";
 
 class App extends Component {
   state = {
-    pokemonUrl: 'http://pokeapi.co/api/v2/pokemon?offset0&limit=807',
-    pokemon: []
-  }
+    pokemonListUrl: "https://pokeapi.co/api/v2/pokemon?offset0&limit=802",
+    pokemonUrl: "https://pokeapi.co/api/v2/pokemon/",
+    pokemon: [],
+    pokemonList: []
+  };
 
   componentDidMount() {
     this.getPokemon();
   }
 
-  getPokemon() {
+  getPokemon = () => {
     axios
-    .get(this.state.pokemonUrl)
-    .then(res => {
-      this.setState({ pokemon: res.data.results })
-      console.log(res.data.results);
-    })
-    .catch(err => console.log('Error: ', err))
-  }
+      .get(this.state.pokemonListUrl)
+      .then(res => {
+        this.setState({ pokemonList: res.data.results });
+        console.log(res.data.results);
+      })
+      .catch(err => console.log("Error: ", err));
+  };
 
-  // handleClick(id) {
-  //   fetch(`http://pokeapi.co/api/v2/pokemon/${id}/`)
-  //   .then(res => res.json())
-  //   .then(data => {
-  //     const pokemon = new Pokemon(data);
+  handleClick = event => {
+    axios
+      .get(this.state.pokemonUrl + `${event.currentTarget.value}`)
+      .then(res => {
+        this.setState({ pokemon: new Pokemon(res.data) });
+        console.log(this.state.pokemon);
+      })
+      .catch(err => console.log(err));
+  };
 
-  //     this.setState({ pokemon });
-  //   })
-  //   .catch(err => console.log(err));
-  // }
-
-  render(){
+  render() {
     return (
       <div className="App">
-        <PokeList 
+        <PokeList
           handleClick={this.handleClick}
-          pokemon={this.state.pokemon} 
+          pokemonList={this.state.pokemonList}
         />
         <DetailView pokemon={this.state.pokemon} />
       </div>
