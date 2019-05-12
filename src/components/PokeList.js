b56a9cd1f09839c2f9f6392c5f5f0f4a6730c26e
Fred <sohnf90@yahoo.com>
2019-05-12 01:34:17 -0700

    Add click handler props, value, and capitalize function

--- src/components/PokeList.js
+++ src/components/PokeList.js
@@ -1,22 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './styles/PokeList.css';
 
 import PokeCell from './PokeCell';
 
-const PokeList = props => {
-    return(
-        <section className="poke-list">
-            {props.pokemon.map((pokemon, index) => {
-                return(
-                    <PokeCell 
-                        key={index}
-                        name={pokemon.name}
-                        pokemonSprite={index + 1}
-                    />
-                )
-            })}
-        </section>
-    )
+class PokeList extends Component {
+    capitalize(pokemonName) {
+        let pokemonLetterArray = pokemonName.split("");
+        pokemonLetterArray[0] = pokemonLetterArray[0].toUpperCase();
+        let pokemonWhole = pokemonLetterArray.join("");
+        return pokemonWhole;
+    }
+    render(){
+        return(
+            <section className="poke-list">
+                {this.props.pokemonList.map((pokemon, index) => {
+                    return(
+                        <PokeCell 
+                            key={index}
+                            pokemonName={this.capitalize(pokemon.name)}
+                            value={pokemon.name}
+                            pokemonSprite={index + 1}
+                            handleClick={this.props.handleClick}
+                        />
+                    )
+                })}
+            </section>
+        )
+    }
 }
 
 export default PokeList;
