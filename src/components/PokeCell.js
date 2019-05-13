bf77496cee5e3878f112d8c618d2fe42a52ea901
Fred <sohnf90@yahoo.com>
2019-05-13 14:11:32 -0700

    Fix code spacing

--- src/components/PokeCell.js
+++ src/components/PokeCell.js
@@ -1,23 +1,23 @@
-import React from 'react';
-import './styles/PokeCell.css';
+import React from "react";
+import "./styles/PokeCell.css";
 
 const PokeCell = props => {
-    return (
-        <button 
-            className="poke-cell"
-            onClick={props.handleClick}
-            value={props.value}
-        >
-            <img 
-                className="poke-sprite"
-                src={require(`../Pokemon/${props.pokemonSprite}.png`)} 
-                alt={props.value}
-            />
-            <p className="poke-name">
-                <strong>{props.pokemonName}</strong>
-            </p>
-        </button>
-    )
-}
+  return (
+    <button
+      className="poke-cell"
+      onClick={props.handleClick}
+      value={props.value}
+    >
+      <img
+        className="poke-sprite"
+        src={require(`../Pokemon/${props.pokemonSprite}.png`)}
+        alt={props.value}
+      />
+      <p className="poke-name">
+        <strong>{props.pokemonName}</strong>
+      </p>
+    </button>
+  );
+};
 
 export default PokeCell;
