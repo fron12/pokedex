e84c35a91c01a8014b3796718aa6fcf9d446604e
Fred <sohnf90@yahoo.com>
2019-05-12 19:42:30 -0700

    Add input for filtering, add function that gets pokemon number from the pokemon url

--- src/components/PokeList.js
+++ src/components/PokeList.js
@@ -10,16 +10,32 @@ class PokeList extends Component {
         let pokemonWhole = pokemonLetterArray.join("");
         return pokemonWhole;
     }
+    accessUrlNumber(url){
+        let strArray = url.split("");
+        let checkArray = [];
+        for(let i = 34; i < strArray.length; i++){
+            if(parseInt(strArray[i]) >= 0){
+                checkArray.push(parseInt(strArray[i]));
+            }
+        }
+        let strNumber = checkArray.join("");
+        return strNumber;
+    }
+
     render(){
         return(
             <section className="poke-list">
+                <input 
+                    className="poke-filter" 
+                    onChange={this.props.handleChange}
+                />
                 {this.props.pokemonList.map((pokemon, index) => {
                     return(
                         <PokeCell 
                             key={index}
                             pokemonName={this.capitalize(pokemon.name)}
                             value={pokemon.name}
-                            pokemonSprite={index + 1}
+                            pokemonSprite={this.accessUrlNumber(pokemon.url)}
                             handleClick={this.props.handleClick}
                         />
                     )
