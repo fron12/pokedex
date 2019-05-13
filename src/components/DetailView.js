8ee75c655051516b0e47227b2fd42adfc7389f98
Fred <sohnf90@yahoo.com>
2019-05-13 13:10:27 -0700

    Added capitalizeArrayItems function to capitalize the pokemon types if a pokemon had more than one type

--- src/components/DetailView.js
+++ src/components/DetailView.js
@@ -8,8 +8,29 @@ class DetailView extends Component {
         let pokemonWhole = pokemonLetterArray.join("");
         return pokemonWhole;
     }
+
+    capitalizeArrayItems(types) {
+        let typesArray = [];
+        let splitTypes = types.split(",");
+        if(splitTypes.length > 1) {
+            for(let i = 0; i < splitTypes.length; i++){
+                let specificSplitType = splitTypes[i].split("");
+                specificSplitType[0] = specificSplitType[0].toUpperCase();
+                let specificJoinedType = specificSplitType.join("");
+                typesArray.push(specificJoinedType);
+            }
+            return typesArray.join(", ");
+        }
+        return this.capitalize(types);
+    }
+
+    colorText(text){
+        
+    }
+
     render(){
         const {id, name, sprite, type } = this.props.pokemon;
+        const capitalizedType = this.capitalizeArrayItems(`${type}`);
         return(
             <section className="detail-view">
                 <div className="sprite-wrapper">
@@ -17,13 +38,20 @@ class DetailView extends Component {
                 </div>
                 <div className="data-wrapper">
                     <h1 className="data-name">ID: {id} Name: {this.capitalize(`${name}`)}</h1>
-                    <p className="data-char">Type: {this.capitalize(`${type}`)}</p>
-                    <p className="data-char">Description:</p>
+                    <p 
+                        className="data-char"
+                        style={{}}
+                    >
+                        <strong>Type:</strong> {capitalizedType}
+                    </p>
+                    <p className="data-char"><strong>Description:</strong><br/> {this.props.description}</p>
                     {/* For description look up flavor-text at url api/v2/pokemon-species/name
                     and filter by language Ex: [0].language.name === "en" && 
-                    [0].version.name === "alpha-sapphire" */}
-                    {/* To Do: Iterate through and return multiple types if possible and
-                    add pokemon description. */}
+                    [0].version.name === "alpha-sapphire" 
+                    Example of Pokemon Type access: pokemonUrl/charizard/ */}
+                    {/* To-Do: filter for english language pokemon description,
+                                color types like green for grass, blue for water.
+                    */}
                 </div>
             </section>
         );
