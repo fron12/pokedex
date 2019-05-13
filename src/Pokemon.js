b4bc36beba56796a86c08756386be46ea525a039
Fred <sohnf90@yahoo.com>
2019-05-13 13:06:18 -0700

    Changed this.type to map over all available types

--- src/Pokemon.js
+++ src/Pokemon.js
@@ -3,7 +3,9 @@ class Pokemon {
         this.id = data.id;
         this.name = data.name;
         this.sprite = data.sprites.front_default;
-        this.type = data.types[0].type.name;
+        this.type = data.types.map(type => {
+            return type.type.name;
+        });
     }
 }
 
