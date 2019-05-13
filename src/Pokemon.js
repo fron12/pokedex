2242536ae178ce46d8632d27b57dfd8846d9056f
Fred <sohnf90@yahoo.com>
2019-05-13 14:05:23 -0700

    Fixed code spacing

--- src/Pokemon.js
+++ src/Pokemon.js
@@ -1,12 +1,12 @@
 class Pokemon {
-    constructor(data){
-        this.id = data.id;
-        this.name = data.name;
-        this.sprite = data.sprites.front_default;
-        this.type = data.types.map(type => {
-            return type.type.name;
-        });
-    }
+  constructor(data) {
+    this.id = data.id;
+    this.name = data.name;
+    this.sprite = data.sprites.front_default;
+    this.type = data.types.map(type => {
+      return type.type.name;
+    });
+  }
 }
 
 export default Pokemon;
